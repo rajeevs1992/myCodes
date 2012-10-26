a3bbfb8b7320bfeab43211d4985201c9ec47a74f
Rajeev S <rajeevs1992@gmail.com>
2012-10-26 10:52:22 +0530

    completed fibonacci prolog

--- prolog/fibo.pl
+++ prolog/fibo.pl
@@ -1,2 +1,12 @@
-X=[1,1]
-Y
+fibo(Sum,Prev,Count):-
+	Sum=2,
+	write('1 1 '),
+	C is Count-2,
+	fibo2(Sum,Prev,C).
+fibo2(Sum,Prev,Count):-
+	Count > 0,
+	D is Sum+Prev,
+	write(D),
+	write(' '),
+	C is Count-1,
+	fibo2(D,Sum,C).
