70d66f3fe1540e1e591b138cbf22be9a279d551c
Rajeev S <rajeevs1992@gmail.com>
2012-11-01 14:06:49 +0530

    documented lca.java

--- java/lca/lca.java
+++ java/lca/lca.java
@@ -1,4 +1,17 @@
+/*
+	Concurrent LCA computation
+	Algorithm:
+		*Read the tree
+		*Read the 2 nodes whose LCA must be computed a,b and a < b
+		*Travaerse the tree starting from root
+		*Create 2 threads,one searching for "a" in currentNode.left and other "b" in currentNode.right
+		*If found,set corresponding flags "af" (a.k.a "a found") or bf
+		*The current node is LCA of a and b if af=bf=true,if found exit.
+		*If af is true and bf is false,it means that a and b lie on the left subtree,hence repeat with left child of currentNode as root
+		*Else repeat with right child of currentNode as root
+*/
 import java.io.*;
+
 class Node
 {
 	int data;
